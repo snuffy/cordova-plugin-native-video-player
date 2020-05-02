7d3b73710ce4fdebdde51482e5ee1ceea2c631da
snuffy <snuffy.tokyo@gmail.com>
2020-05-02 11:10:57 +0900

    add: ios player

--- src/ios/CDVNativeVideoPlayer.swift
+++ src/ios/CDVNativeVideoPlayer.swift
@@ -1,17 +1,68 @@
-
+import UIKit
+import AVFoundation
+import PIPKit
+import SJVideoPlayer
 
 @objc(CDVNativeVideoPlayer) class CDVNativeVideoPlayer : CDVPlugin {
-  override func pluginInitialize() {};
 
-  @objc func start(_ command: CDVInvokedUrlCommand) {
-    UIApplication.shared.isIdleTimerDisabled = true
-    let result = CDVPluginResult(status: CDVCommandStatus_OK)
-    commandDelegate.send(result, callbackId: command.callbackId)
-  }
+    override func pluginInitialize() {
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            UIApplication.shared.beginReceivingRemoteControlEvents()
+            SJVideoPlayer.update { (settings) in
+                settings.progress_thumbSize = 12;
+            }
+        }
+        catch {
+            // TODO error
+        }
 
-  @objc func stop(_ command: CDVInvokedUrlCommand) {
-    UIApplication.shared.isIdleTimerDisabled = false
-    let result = CDVPluginResult(status: CDVCommandStatus_OK)
-    commandDelegate.send(result, callbackId: command.callbackId)
-  }
+    };
+
+    @objc func start(_ command: CDVInvokedUrlCommand) {
+        
+        // get media
+        let data = command.argument(at: 0) as! [[String:String]]
+        var playlist: [MediaItem] = []
+        data.forEach { (item) in
+            guard let title = item["title"]?.removingPercentEncoding,
+                    let album = item["album"]?.removingPercentEncoding,
+                    var source = item["source"]?.removingPercentEncoding
+            else {
+                return
+            }
+            
+            // get source
+            if let range = source.range(of: "file://") {
+                source.replaceSubrange(range, with: "")
+                print(source)
+                let u = URL(fileURLWithPath:source)
+                let m = MediaItem(title: title, album: album, source: u)
+                playlist.append(m)
+            }
+            
+        }
+
+        let vc = VGMediaViewController()
+
+        vc.playlist = playlist
+        PIPKit.show(with: vc)
+        vc.stopPIPMode()
+        self.viewController.presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+
+    @objc func stop(_ command: CDVInvokedUrlCommand) {
+        UIApplication.shared.isIdleTimerDisabled = false
+        let result = CDVPluginResult(status: CDVCommandStatus_OK)
+        commandDelegate.send(result, callbackId: command.callbackId)
+    }
+    
+    
+}
+
+
+extension Notification.Name {
+    static let closeButton = Notification.Name("notifyName")
 }
---
+++ src/ios/CDVNativeVideoPlayerViewController.swift
@@ -0,0 +1,294 @@
+//
+//  CDVNativeVideoPlayerViewController.swift
+//  Example
+//
+//  Created by snuffy on 2020/04/30
+//  Copyright © 2020年 snuffy. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import PIPKit
+import MediaPlayer
+import SJUIKit
+import SJVideoPlayer
+import Masonry
+
+public struct MediaItem {
+    var title: String
+    var album: String
+    var source: URL
+}
+
+class VGMediaViewController: UIViewController, ConstraintRelatableTarget, PIPUsable {
+    
+    var player:SJVideoPlayer!
+    let closeButton = UIButton(type: .custom)
+    let pipButton = UIButton(type: .custom)
+    let shrinkScreenImage = SJVideoPlayerResourceLoader.imageNamed("sj_video_player_shrinkscreen")
+    let fullScreenImage = SJVideoPlayerResourceLoader.imageNamed("sj_video_player_fullscreen")
+    
+    let media = MediaItem(title: "タイトル1", album: "アルバム1", source: URL(string: "http://www.hochmuth.com/mp3/Haydn_Cello_Concerto_D-1.mp3")! )
+    let media2 = MediaItem(title: "タイトル2", album: "アルバム2", source: URL(string: "https://dh2.v.netease.com/2017/cg/fxtpty.mp4")!)
+    
+    var playlist:[MediaItem] = []
+    
+    var currentIndex = 0
+    
+    @IBOutlet weak var containerView: UIView!
+    
+    override func viewDidLoad() {
+
+//        playlist.append(media)
+//        playlist.append(media2)
+        
+        player = SJVideoPlayer()
+        player.pauseWhenAppDidEnterBackground = false;
+        player.view.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(self.player.view)
+        self.player.view.mas_makeConstraints { (make) in
+            make?.edges.setOffset(0)
+        }
+        
+        
+        let first = playlist[currentIndex]
+        player.urlAsset = SJVideoPlayerURLAsset(url: first.source)
+        
+        // プレイヤーの設定
+        setupPlayerSettings()
+        // クローズボタン
+        createCloseButton()
+        // PIP ボタン
+        createPipButton()
+        // remote commandEvent を作成
+        addRemoteCommandEvent()
+        // pla
+        updateNowPlayingInfo();
+        
+        // 再生が終了したらよばれれる
+        player.playbackObserver.playbackDidFinishExeBlock = {[weak self] _ in
+            guard let self = self else {return}
+            self.nextMedia()
+            return
+            
+        }
+    }
+    
+    private func createCloseButton() {
+        self.closeButton.setTitle("閉じる", for: .normal)
+        self.closeButton.titleLabel?.font   = UIFont.boldSystemFont(ofSize: 20.0)
+        self.closeButton.tintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        self.closeButton.addTarget(self, action: #selector(self.tappedClose(_:)), for: .touchUpInside)
+        self.closeButton.sizeToFit();
+        self.view.addSubview(self.closeButton)
+        self.closeButton.snp.makeConstraints { (make) in
+            if #available(iOS 11, *) {
+                make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(4)
+            } else {
+                make.top.equalTo(self.view)
+            }
+            make.left.equalTo(self.view.snp.left).offset(16)
+
+        }
+    }
+    
+    private func createPipButton() {
+        self.pipButton.setImage(shrinkScreenImage, for: .normal)
+        self.pipButton.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.7956095951)
+        self.pipButton.layer.cornerRadius = 20
+        self.pipButton.titleEdgeInsets = UIEdgeInsetsMake(2.0, 6.0, 2.0, 6.0)
+        self.pipButton.tintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        self.pipButton.addTarget(self, action: #selector(self.tapToPip(_:)), for: .touchUpInside)
+//        self.pipButton.sizeToFit();
+        self.view.addSubview(self.pipButton)
+        self.pipButton.snp.makeConstraints { (make) in
+            if #available(iOS 11, *) {
+                make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(4)
+            } else {
+                make.top.equalTo(self.view)
+            }
+            make.width.equalTo(40)
+            make.height.equalTo(40)
+            make.left.equalTo(self.view.snp.right).offset(-44)
+
+        }
+    }
+    
+    private func setupPlayerSettings() {
+        player.gestureControl.supportedGestureTypes = SJPlayerGestureTypeMask_All
+        player.rateWhenLongPressGestureTriggered = 1.5
+
+        player.showMoreItemToTopControlLayer = true;
+        player.defaultEdgeControlLayer.isHiddenBackButtonWhenOrientationIsPortrait = true
+        
+        // 画面広がるボタンの削除
+        player.defaultEdgeControlLayer.bottomAdapter.removeItem(forTag: SJEdgeControlLayerBottomItem_FullBtn)
+        
+        // 設定ボタンの配置
+        let moreImage = SJVideoPlayerResourceLoader.imageNamed("sj_video_player_more")
+        let settingSwitchItem = SJEdgeControlButtonItem(image: moreImage, target: self, action: #selector(openMore), tag: 50)
+        
+        
+        player.defaultEdgeControlLayer.bottomAdapter.add(settingSwitchItem)
+        player.defaultEdgeControlLayer.bottomAdapter.reload()
+        // 自動回転のオフ
+        player.rotationManager.isDisabledAutorotation = true
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.player.vc_viewDidAppear()
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.player.vc_viewWillDisappear()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        self.player.vc_viewDidDisappear()
+    }
+    
+    @objc func tappedClose(_ sender: UIButton) {
+        self.player.controlLayerNeedAppear()
+        PIPKit.dismiss(animated: true)
+    }
+    
+    @objc func tapToPip(_ sender: UIButton) {
+        if PIPKit.isPIP {
+            stopPIPMode()
+            pipButton.setImage(shrinkScreenImage, for: .normal)
+            closeButton.isHidden = false;
+            player.defaultEdgeControlLayer.isHidden = false
+            player.gestureControl.supportedGestureTypes = SJPlayerGestureTypeMask_All
+        } else {
+            startPIPMode()
+            pipButton.setImage(fullScreenImage, for: .normal)
+            closeButton.isHidden = true;
+            player.defaultEdgeControlLayer.isHidden = true
+            player.gestureControl.supportedGestureTypes = SJPlayerGestureTypeMask_DoubleTap
+        }
+    }
+    
+    @objc func openMore() {
+        self.player.switcher.switchControlLayer(forIdentitfier: SJControlLayer_MoreSettting)
+    }
+    
+    
+    func nextMedia() {
+        if (playlist.count-1 == currentIndex) {return}
+        currentIndex += 1
+        // なければ何もしない
+        let next = self.playlist[self.currentIndex]
+        self.player.urlAsset = SJVideoPlayerURLAsset(url: next.source)
+        self.player.play()
+        
+        // update info
+        updateNowPlayingInfo()
+        updateRemoteCommandCenter()
+    }
+    
+    func prevMedia() {
+        if (currentIndex == 0) {return}
+        currentIndex -= 1
+        let prev = self.playlist[self.currentIndex]
+        self.player.urlAsset = SJVideoPlayerURLAsset(url: prev.source)
+        self.player.play()
+        
+        // update info
+        updateNowPlayingInfo()
+        updateRemoteCommandCenter()
+    }
+    
+    func updateRemoteCommandCenter() {
+        let commandCenter = MPRemoteCommandCenter.shared()
+        let hasNext = (playlist.count-1 == currentIndex) ? false : true
+        let hasPrev = (currentIndex == 0) ? false : true
+        
+        commandCenter.nextTrackCommand.isEnabled = hasNext
+        commandCenter.previousTrackCommand.isEnabled = hasPrev
+    }
+    
+    func updateNowPlayingInfo() {
+        let currentMedia = playlist[currentIndex]
+        let title = currentMedia.title
+        let album = currentMedia.album
+        
+        let nowPlayingInfoCenter = MPNowPlayingInfoCenter.default()
+        var nowPlayingInfo = nowPlayingInfoCenter.nowPlayingInfo ?? [String: Any]()
+        
+        let image = UIImage(named: "ICON") ?? UIImage()
+        let artwork = MPMediaItemArtwork(boundsSize: image.size, requestHandler: {  (_) -> UIImage in
+          return image
+        })
+
+        nowPlayingInfo[MPMediaItemPropertyTitle] = title
+        nowPlayingInfo[MPMediaItemPropertyAlbumTitle] = album
+        nowPlayingInfo[MPMediaItemPropertyArtwork] = artwork
+        nowPlayingInfo[MPNowPlayingInfoPropertyPlaybackRate] = NSNumber(value: 1.0)
+        nowPlayingInfoCenter.nowPlayingInfo = nowPlayingInfo
+    }
+    // Remote Command Event
+    func addRemoteCommandEvent() {
+        UIApplication.shared.beginReceivingRemoteControlEvents()
+        let commandCenter = MPRemoteCommandCenter.shared()
+        commandCenter.playCommand.isEnabled = true
+        commandCenter.pauseCommand.isEnabled = true
+        commandCenter.togglePlayPauseCommand.isEnabled = true
+        commandCenter.skipBackwardCommand.isEnabled = false
+        commandCenter.skipForwardCommand.isEnabled = false
+
+        commandCenter.togglePlayPauseCommand.addTarget(handler: { [unowned self] commandEvent -> MPRemoteCommandHandlerStatus in
+            self.remoteTogglePlayPause(commandEvent)
+            return MPRemoteCommandHandlerStatus.success
+        })
+        commandCenter.playCommand.addTarget(handler: { [unowned self] commandEvent -> MPRemoteCommandHandlerStatus in
+            self.remotePlay(commandEvent)
+            return .success
+        })
+        commandCenter.pauseCommand.addTarget(handler: { [unowned self] commandEvent -> MPRemoteCommandHandlerStatus in
+            self.remotePause(commandEvent)
+            return .success
+        })
+        commandCenter.nextTrackCommand.addTarget(handler: { [unowned self] commandEvent -> MPRemoteCommandHandlerStatus in
+            self.remoteNextTrack(commandEvent)
+            return .success
+        })
+        commandCenter.previousTrackCommand.addTarget(handler: { [unowned self] commandEvent -> MPRemoteCommandHandlerStatus in
+            self.remotePrevTrack(commandEvent)
+            return .success
+        })
+        
+        updateRemoteCommandCenter()
+    }
+
+    func remoteTogglePlayPause(_ event: MPRemoteCommandEvent) {
+        if (player.isPlaying) {
+            player.pause()
+        }
+        else {
+            player.play()
+        }
+    }
+
+    func remotePlay(_ event: MPRemoteCommandEvent) {
+        player.play()
+    }
+
+    func remotePause(_ event: MPRemoteCommandEvent) {
+        player.pause()
+    }
+
+    func remoteNextTrack(_ event: MPRemoteCommandEvent) {
+        nextMedia()
+    }
+
+    func remotePrevTrack(_ event: MPRemoteCommandEvent) {
+        prevMedia()
+    }
+    
+    
+}
