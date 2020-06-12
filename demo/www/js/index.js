0dab3c51553392a6184e5cffb74321fae08fe9bb
snuffy <snuffy.tokyo@gmail.com>
2020-06-13 08:08:56 +0900

    fix demo

--- demo/www/js/index.js
+++ demo/www/js/index.js
@@ -1,58 +1,34 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 var app = {
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
-        NativeVideoPlayer.start([
-            {
-                title: encodeURIComponent('jsからタイトル1'),
-                album: encodeURIComponent('jsからアルバム1'),
-                source: encodeURIComponent('http://www.hochmuth.com/mp3/Haydn_Cello_Concerto_D-1.mp3')
-            },
-            {
-                title: 'jsからタイトル2',
-                album: 'jsからアルバム2',
-                source: encodeURIComponent('https://dh2.v.netease.com/2017/cg/fxtpty.mp4')
-            }, 
-        ])
+        
+        // open video player
+        const videoBtn = document.querySelector('.openVideoBtn');
+        videoBtn.addEventListener('click', openVideo);
     },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
 };
 
 app.initialize();
+
+function openVideo () {
+    NativeVideoPlayer.start([
+        // 音声ファイル
+        {
+            title: 'タイトル1',
+            album: 'アルバム1',
+            source: encodeURIComponent('http://www.hochmuth.com/mp3/Haydn_Cello_Concerto_D-1.mp3')
+        },
+        // 動画ファイル
+        {
+            title: 'タイトル2',
+            album: 'アルバム2',
+            source: encodeURIComponent('https://dh2.v.netease.com/2017/cg/fxtpty.mp4')
+        }, 
+    ])
+}
