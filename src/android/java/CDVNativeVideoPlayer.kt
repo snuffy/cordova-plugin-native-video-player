15fbdb5d8235b809a15dc34c0d94b82368aa5ff5
snuffy <snuffy.tokyo@gmail.com>
2020-06-21 13:23:27 +0900

    fix: indent

--- src/android/java/CDVNativeVideoPlayer.kt
+++ src/android/java/CDVNativeVideoPlayer.kt
@@ -1,11 +1,16 @@
 package jp.rabee
 
+import android.app.PendingIntent
 import android.content.Intent
-import org.apache.cordova.*
+import android.graphics.Bitmap
 import android.util.Log
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.ui.PlayerNotificationManager
 import com.google.gson.GsonBuilder
-import org.json.*
+import org.apache.cordova.*
+import org.json.JSONArray
 import java.net.URLDecoder
+
 
 class CDVNativeVideoPlayer : CordovaPlugin() {
 
@@ -27,33 +32,33 @@ class CDVNativeVideoPlayer : CordovaPlugin() {
         }
 
         return result
-    }
-
-    // プレイヤーの再生スタート
-    private fun start(callbackContext: CallbackContext, params: JSONArray): Boolean {
-        cordova.activity.run {
-            GsonBuilder().create().also { gson ->
-                val items = gson.fromJson(params.toString(), Array<MediaItem>::class.java).toList()
-                items.forEach {
-                    Log.d(TAG, "MediaItem: ${URLDecoder.decode(it.source, "UTF-8")}")
-                }
-
-                val intent = Intent(cordova.activity.application, PlayerActivity::class.java)
-                intent.putExtra(MediaItem.MEDIA_ITEMS_EXTRA, gson.toJson(items))
-                startActivity(intent)
-
-                val pluginResult = PluginResult(PluginResult.Status.OK, true);
-                callbackContext.sendPluginResult(pluginResult);
-            } ?: run {
-
-                val pluginResult = PluginResult(PluginResult.Status.ERROR, false);
-                callbackContext.sendPluginResult(pluginResult);
-
-                return false;
-            }
         }
-        return true
-    }
+
+        // プレイヤーの再生スタート
+        private fun start(callbackContext: CallbackContext, params: JSONArray): Boolean {
+            cordova.activity.run {
+                GsonBuilder().create().also { gson ->
+                    val items = gson.fromJson(params.toString(), Array<MediaItem>::class.java).toList()
+                    items.forEach {
+                        Log.d(TAG, "MediaItem: ${URLDecoder.decode(it.source, "UTF-8")}")
+                    }
+
+                    val intent = Intent(cordova.activity.application, PlayerActivity::class.java)
+                    intent.putExtra(MediaItem.MEDIA_ITEMS_EXTRA, gson.toJson(items))
+                    startActivity(intent)
+
+                    val pluginResult = PluginResult(PluginResult.Status.OK, true);
+                    callbackContext.sendPluginResult(pluginResult);
+                } ?: run {
+
+                    val pluginResult = PluginResult(PluginResult.Status.ERROR, false);
+                    callbackContext.sendPluginResult(pluginResult);
+
+                    return false;
+                }
+            }
+            return true
+        }
 
     companion object {
         private const val TAG = "CDVNativeVideoPlayer"
